f3911fca171414719679fea6bf369df6a4fe6d47
Preston McNabb <prestonmcnabb@Prestons-MacBook-Pro.local>
2017-12-12 15:13:25 -0600

    Pre Final push, almost done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,63 +1,90 @@
 //javascript tracker//
 
-//Initialize firebase//
+// Initialize Firebase
 var config = {
-    apiKey: "AIzaSyDLswEA6G8Thai3AC2xBF7QSof-rQx3f6U",
-    authDomain: "traintracker-35f50.firebaseapp.com",
-    databaseURL: "https://traintracker-35f50.firebaseio.com",
-    projectId: "traintracker-35f50",
-    storageBucket: "traintracker-35f50.appspot.com",
-    messagingSenderId: "1057623940302"
+    apiKey: "AIzaSyBnWwJharX4_Ss9Wan3oUz1ak7Zk3vWhkQ",
+    authDomain: "fir-practice-1b721.firebaseapp.com",
+    databaseURL: "https://fir-practice-1b721.firebaseio.com",
+    projectId: "fir-practice-1b721",
+    storageBucket: "fir-practice-1b721.appspot.com",
+    messagingSenderId: "355874204281"
   };
   firebase.initializeApp(config);
 
-
+  //Sets up variable database to connect with firebase//
   var database = firebase.database();
 
-  var name = "";
-  var destination = "";
-  var frequencey;
-  var nextTrain = moment().format("HH:mm");
+  //Global variable
+  var timeRemainder = 0;
 
-  database.ref().on("child_added", function (snapshot){
+  database.ref().orderByChild("inputDest").on("child_added", function (snapshot){
       console.log('here')
+
+      //First train//
+      var firstTimeCoverted = moment(snapshot.val().inputFirst, "HHmm").subtract(1,"years");
+
+      //Creates a variable for the current time//
+      var currentTime = moment();
+      console.log("Current Time: " + currentTime);
+
+      //Creates a variable for the differnece between current and first time//
+      var diffTime = currentTime.diff(moment(firstTimeCoverted), "minutes");
+      console.log("Difference in Time:" + diffTime);
+
+      timeRemainder = diffTime % snapshot.val().inputFreq;
+      console.log("Time Remaining: " + timeRemainder);
+
+      //Variable to capture the value of input frequency//
+      var timeFrequency = snapshot.val().inputFreq;
+      console.log("Time Frequency: " + timeFrequency);
+
+      //Varialbe that maths the number of mins until next train//
+      var minutesAway = timeFrequency - timeRemainder;
+      console.log("Minutes away: " + minutesAway);
+
+      //Variable to math the minutes away//
+      var nextArrival = moment().add(minutesAway, "minutes");
+      console.log("Arrive at: " + moment(nextArrival).format("HHmm"));
+
+      //Adds our inputs to the table//
       $('#dostuff').append(
           `<tr class="active>
-          <td id="table-name">${snapshot.val().inputTrain}</td>
+          <td id="table-name">${snapshot.val().inputName}</td>
+          <td id="table-name">${snapshot.val().inputName}</td>
           <td id="table-destination">${snapshot.val().inputDest}</td>
           <td id="table-frequency">${snapshot.val().inputFreq}</td>
-          <td id="table-next">${snapshot.val().inputNext}</td>
-          <td id="table-away"></td>
-          </td>
-
-          `
+          <td id="table-next">${moment(nextArrival).format("HHmm")}</td>
+          <td id="table-away">${minutesAway}</td>
+          </td>`
       )
-  })
+  });
 
   //submit form variables and pushing to firebase//
   $("#submit-train").on("click", function(event){
       event.preventDefault();
 
-        var inputTrain = $("#train-name").val().trim();
+        var inputName = $("#train-name").val().trim();
         var inputDest = $("#train-dest").val().trim();
-        var inputNext = $("#train-time").val().trim();
+        var inputFirst = $("#train-time").val().trim();
         var inputFreq = $("#train-freq").val().trim();
 
     //console.log to check if inputs are working//
     console.log ({
-        inputTrain: inputTrain,
+        inputName: inputName,
         inputDest: inputDest,
-        inputNext: inputNext,
+        inputFirst: inputFirst,
         inputFreq: inputFreq
     })
 
     //submits our user inputs to firebase//
     database.ref().push({
-        inputTrain: inputTrain,
+        inputName: inputName,
         inputDest: inputDest,
-        inputNext: inputNext,
+        inputFirst: inputFirst,
         inputFreq: inputFreq
-    });
+    })
+    //Clears inputs
+    $('input').val('');
 
 
   });
