20d5aaaa930f4defb61b2e7a7415c87326a1dec8
Preston McNabb <prestonmcnabb@Prestons-MacBook-Pro.local>
2017-12-11 18:48:30 -0600

    Next push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,16 @@
 //javascript tracker//
 
-//initialize firebase//
+//Initialize firebase//
 var config = {
     apiKey: "AIzaSyDLswEA6G8Thai3AC2xBF7QSof-rQx3f6U",
     authDomain: "traintracker-35f50.firebaseapp.com",
     databaseURL: "https://traintracker-35f50.firebaseio.com",
     projectId: "traintracker-35f50",
-    storageBucket: "",
+    storageBucket: "traintracker-35f50.appspot.com",
     messagingSenderId: "1057623940302"
   };
   firebase.initializeApp(config);
+
 
   var database = firebase.database();
 
@@ -22,23 +23,41 @@ var config = {
       console.log('here')
       $('#dostuff').append(
           `<tr class="active>
-          <td id="table-name">${snapshot.val().inputName}</td>
-          <td id="table-destination">${snapshot.val().inputRole}</td>
-          <td id="table-frequency">${snapshot.val().startDate}</td>
-          <td id="table-next"></td>
-          <td id="table-away">${snapshot.val().inputRate}</td>
+          <td id="table-name">${snapshot.val().inputTrain}</td>
+          <td id="table-destination">${snapshot.val().inputDest}</td>
+          <td id="table-frequency">${snapshot.val().inputFreq}</td>
+          <td id="table-next">${snapshot.val().inputNext}</td>
+          <td id="table-away"></td>
           </td>
 
           `
       )
   })
 
+  //submit form variables and pushing to firebase//
   $("#submit-train").on("click", function(event){
       event.preventDefault();
 
-        var inputTrain
-        var inputDest
-        var inputNext
-        var inputFreq
+        var inputTrain = $("#train-name").val().trim();
+        var inputDest = $("#train-dest").val().trim();
+        var inputNext = $("#train-time").val().trim();
+        var inputFreq = $("#train-freq").val().trim();
 
-  })
+    //console.log to check if inputs are working//
+    console.log ({
+        inputTrain: inputTrain,
+        inputDest: inputDest,
+        inputNext: inputNext,
+        inputFreq: inputFreq
+    })
+
+    //submits our user inputs to firebase//
+    database.ref().push({
+        inputTrain: inputTrain,
+        inputDest: inputDest,
+        inputNext: inputNext,
+        inputFreq: inputFreq
+    });
+
+
+  });
